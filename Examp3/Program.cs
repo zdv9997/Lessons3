223a14aa6096bb531aa8ab59a5e69432356bfec5
Dmitriy <zdv9997@mail.ru>
2022-11-12 22:41:44 +0200

    добавили рандом в сложение

--- Examp3/Program.cs
+++ Examp3/Program.cs
@@ -3,7 +3,11 @@
 //int numberA = Console.ReadLine();
 //Console.Write("B= ");
 //int numberB = Console.ReadLine();
-int numberA = 3;
-int numberB = 21;
+int numberA = new Random().Next(1, 20);
+int numberB = new Random().Next(1, 20);
+Console.Write("Число A = ");
+Console.WriteLine(numberA);
+Console.Write("Число B = ");
+Console.WriteLine(numberB);
 Console.Write("Сумма А и В = ");
 Console.WriteLine(numberA + numberB);
