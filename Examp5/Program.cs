65e8ddbefc95d2aa8f530e812334b004b8322b24
Dmitriy <zdv9997@mail.ru>
2022-11-12 22:54:59 +0200

    сделали ввод имени и реакцию

---
+++ Examp5/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, вот она!");
+}
+else
+{
+    Console.Write("Капец, это не она! это ");
+    Console.WriteLine(username);
+}
