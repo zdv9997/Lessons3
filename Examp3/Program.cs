220b1dac42690eb16f34321161cd097324153868
Dmitriy <zdv9997@mail.ru>
2022-11-12 21:39:48 +0200

    сделали сложение двух чисел

---
+++ Examp3/Program.cs
@@ -0,0 +1,9 @@
+﻿//Console.WriteLine("Введите числа А и В: ");
+//Console.Write("A= ");
+//int numberA = Console.ReadLine();
+//Console.Write("B= ");
+//int numberB = Console.ReadLine();
+int numberA = 3;
+int numberB = 21;
+Console.Write("Сумма А и В = ");
+Console.WriteLine(numberA + numberB);
