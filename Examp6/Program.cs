e8facf681d235c470d0d90f3b3bd6a7b8ffe409a
Dmitriy <zdv9997@mail.ru>
2022-11-15 16:21:51 +0200

    сделали вычисление max

---
+++ Examp6/Program.cs
@@ -0,0 +1,12 @@
+﻿int numberA = 1;
+int numberB = 8;
+int numberC = 3;
+int numberD = 2;
+int numberE = 6;
+int max = numberA;
+if (numberB > max) max = numberB;
+if (numberC > max) max = numberC;
+if (numberD > max) max = numberD;
+if (numberE > max) max = numberE;
+Console.Write("max = ");
+Console.WriteLine(max);
